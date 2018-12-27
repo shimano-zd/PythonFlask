f7436d90730ffa8b6212fb4c1a9fcc8415365780
Sime Martinov <sime.martinov@rathmann.hr>
2018-12-27 11:04:14 +0100

    first build

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import requests
+from datetime import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route('/weather')
+def index():
+    # 2643743 London, 3186952 Zadar
+    return city(3186952)
+
+@app.route('/<id>')
+def city(id):
+    parameters = { 'appid': '7f1839b423ed0ec9c2c366cab3867ca2',
+    'id': id, 'units': 'metric', 'lang': 'hr' }
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    url2 = 'https://api.openweathermap.org/data/2.5/forecast'
+    response = requests.get(url, params=parameters)
+    response2 = requests.get(url2, params=parameters)
+    
+    jsonWeather = response.json()
+    jsonForecast = response2.json()
+    
+    dt = datetime.utcfromtimestamp( jsonWeather['dt'] )
+    now = datetime.now()
+    return render_template('weather.html', jsonWeather=jsonWeather, jsonForecast=jsonForecast, 
+        dt=dt, now=now)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
