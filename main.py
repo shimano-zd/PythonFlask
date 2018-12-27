237ec84cba62378e4fe28b84962bfe76e3ffe37a
Sime Martinov <sime.martinov@rathmann.hr>
2018-12-27 14:58:31 +0100

    added risnjak

--- main.py
+++ main.py
@@ -11,37 +11,18 @@ def homepage():
 
     return render_template("index.html", jsonWeather=getWeather(3186952))
  
-@app.route("/test")
-def test():
-    return render_template("test.html")
 
-@app.route('/weather')
-def index():
-    # 2643743 London, 3186952 Zadar
-    return city(3186952)
+@app.route("/risnjak")
+def risnjak():
+    return render_template("risnjak.html", jsonWeather=getWeather(3202104))
 
-@app.route('/<id>')
-def city(id):
-    parameters = { 'appid': '7f1839b423ed0ec9c2c366cab3867ca2',
-    'id': id, 'units': 'metric', 'lang': 'hr' }
-    url = 'https://api.openweathermap.org/data/2.5/weather'
-    url2 = 'https://api.openweathermap.org/data/2.5/forecast'
-    response = requests.get(url, params=parameters)
-    response2 = requests.get(url2, params=parameters)
-    
-    jsonWeather = response.json()
-    jsonForecast = response2.json()
-    
-    dt = datetime.utcfromtimestamp( jsonWeather['dt'] )
-    now = datetime.now()
-    return render_template('weather.html', jsonWeather=jsonWeather, jsonForecast=jsonForecast, 
-        dt=dt, now=now)
+
 
 
 def getWeather(id):
 
     parameters = { 'appid': '7f1839b423ed0ec9c2c366cab3867ca2',
-    'id': id, 'units': 'metric', 'lang': 'hr' }
+    'id': id, 'units': 'metric', 'lang': 'en' }
     url = 'https://api.openweathermap.org/data/2.5/weather'
     
     response = requests.get(url, params=parameters)
